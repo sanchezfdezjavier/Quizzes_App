12b7ce5c6888cf00ee7daff4e3197b586d5e1732
Javier Sánchez Fernández <sanchezfdez.javier@gmail.com>
2020-10-08 10:56:43 +0200

    Initial Commit

---
+++ Quizz_App_V3/Quizz_App_V3App.swift
@@ -0,0 +1,17 @@
+//
+//  Quizz_App_V3App.swift
+//  Quizz_App_V3
+//
+//  Created by Javi Sanchez on 08/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Quizz_App_V3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
